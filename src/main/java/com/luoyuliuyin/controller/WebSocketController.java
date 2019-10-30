b55c2b628af9b13a96ea4a49ec88d15589d4e9d7
weixiang <weixiang>
2019-10-30 22:12:14 +0800

    jingfeng:add

--- src/main/java/com/luoyuliuyin/controller/WebSocketController.java
+++ src/main/java/com/luoyuliuyin/controller/WebSocketController.java
@@ -9,9 +9,10 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by jingfeng on 2017/7/11.
@@ -21,61 +22,80 @@ import java.io.InputStreamReader;
 public class WebSocketController {
     private static final Logger logger = LoggerFactory.getLogger(WebSocketController.class);
 
+    private static Map<Session, Thread> runningMap = new ConcurrentHashMap<>();
+
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
     }
 
     @OnOpen
-    public void onOpen(Session session) throws IOException {
+    public void onOpen(Session session) {
     }
 
     @OnMessage
-    public void onMessage(String message, Session session) throws IOException {
+    public void onMessage(String message, Session session) {
 
-        try {
-            Runtime rt = Runtime.getRuntime();
-            Process p = rt.exec(new String[]{"/bin/sh", "-c", message});
+        Thread thread = new Thread(() -> {
+            try {
+                Runtime rt = Runtime.getRuntime();
+                Process p = rt.exec(new String[]{"/bin/sh", "-c", message});
 
-            InputStream is = p.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String line;
-            logger.info("执行命令开始：{}", message);
-            while ((line = br.readLine()) != null) {
-                logger.info(line);
-                session.getBasicRemote().sendText(line);
-            }
-
-            while (p.isAlive()) {
-                Thread.sleep(100L);
-            }
-
-            if (p.exitValue() != 0) {
-                is = p.getErrorStream();
-                br = new BufferedReader(new InputStreamReader(is));
+                InputStream is = p.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                String line;
+                logger.info("执行命令开始：{}", message);
                 while ((line = br.readLine()) != null) {
-                    logger.error(line);
+                    logger.info(line);
                     session.getBasicRemote().sendText(line);
                 }
-            }
 
-            br.close();
-            is.close();
-        } catch (Exception e) {
-            logger.error("执行命令error:", e);
-            session.getBasicRemote().sendText(e.toString());
-        }
-        logger.info("执行命令结束：{}", message);
-        session.getBasicRemote().sendText("执行结束");
+                while (p.isAlive()) {
+                    Thread.sleep(100L);
+                }
+
+                if (p.exitValue() != 0) {
+                    is = p.getErrorStream();
+                    br = new BufferedReader(new InputStreamReader(is));
+                    while ((line = br.readLine()) != null) {
+                        logger.error(line);
+                        session.getBasicRemote().sendText(line);
+                    }
+                }
+
+                br.close();
+                is.close();
+            } catch (Exception e) {
+                logger.error("执行命令error:", e);
+                if (session.isOpen()) {
+                    session.getAsyncRemote().sendText(e.getMessage());//异步
+                }
+            } finally {
+                logger.info("执行命令结束：{}", message);
+                if (session.isOpen()) {
+                    session.getAsyncRemote().sendText("执行结束");
+                }
+            }
+        });
+        thread.start();
+        runningMap.put(session, thread);
     }
 
     @OnClose
-    public void onClose() {
-        logger.info("WebSocket close.");
+    public void onClose(Session session) {
+        threadStop(session);
     }
 
     @OnError
-    public void onError(Throwable error) {
-        logger.error("WebSocket error!", error);
+    public void onError(Session session, Throwable error) {
+        threadStop(session);
+    }
+
+    private void threadStop(Session session) {
+        Thread thread = runningMap.get(session);
+        if (thread != null && thread.isAlive()) {
+            thread.interrupt();
+        }
+        runningMap.remove(session);
     }
 }
